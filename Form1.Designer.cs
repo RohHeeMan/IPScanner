a2564360a3372bed23ccc2f6a3519eddb7bcf84f
hmroh <hmroh@DESKTOP-LP1E8EC>
2022-03-03 10:43:42 +0900

    초기 커밋.

---
+++ Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿
+namespace IPScanner
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 필수 디자이너 변수입니다.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 사용 중인 모든 리소스를 정리합니다.
+		/// </summary>
+		/// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form 디자이너에서 생성한 코드
+
+		/// <summary>
+		/// 디자이너 지원에 필요한 메서드입니다. 
+		/// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.button1 = new System.Windows.Forms.Button();
+			this.Schedule_Timer = new System.Windows.Forms.Timer(this.components);
+			this.label1 = new System.Windows.Forms.Label();
+			this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+			this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+			this.label2 = new System.Windows.Forms.Label();
+			this.lbl_FilePath = new System.Windows.Forms.Label();
+			this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+			this.lbl_FileChoice = new System.Windows.Forms.Label();
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.button2 = new System.Windows.Forms.Button();
+			this.lblTmr = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Font = new System.Drawing.Font("굴림", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+			this.button1.Location = new System.Drawing.Point(41, 269);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(335, 60);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "작업 시작";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			this.button1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.button1_MouseMove);
+			// 
+			// Schedule_Timer
+			// 
+			this.Schedule_Timer.Interval = 1000;
+			this.Schedule_Timer.Tick += new System.EventHandler(this.Schedule_Timer_Tick);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("굴림체", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+			this.label1.Location = new System.Drawing.Point(37, 26);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(354, 24);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "작동시간 설정(1Day 1,440분)";
+			// 
+			// numericUpDown1
+			// 
+			this.numericUpDown1.Font = new System.Drawing.Font("굴림체", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+			this.numericUpDown1.Location = new System.Drawing.Point(41, 68);
+			this.numericUpDown1.Maximum = new decimal(new int[] {
+            276447231,
+            23283,
+            0,
+            0});
+			this.numericUpDown1.Name = "numericUpDown1";
+			this.numericUpDown1.Size = new System.Drawing.Size(169, 35);
+			this.numericUpDown1.TabIndex = 2;
+			this.numericUpDown1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+			this.numericUpDown1.ThousandsSeparator = true;
+			this.numericUpDown1.Value = new decimal(new int[] {
+            1440,
+            0,
+            0,
+            0});
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("굴림체", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+			this.label2.Location = new System.Drawing.Point(37, 166);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(249, 24);
+			this.label2.TabIndex = 4;
+			this.label2.Text = "장비 설정 파일 찾기";
+			// 
+			// lbl_FilePath
+			// 
+			this.lbl_FilePath.AutoSize = true;
+			this.lbl_FilePath.Font = new System.Drawing.Font("굴림체", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+			this.lbl_FilePath.Location = new System.Drawing.Point(37, 177);
+			this.lbl_FilePath.Name = "lbl_FilePath";
+			this.lbl_FilePath.Size = new System.Drawing.Size(0, 24);
+			this.lbl_FilePath.TabIndex = 6;
+			// 
+			// imageList1
+			// 
+			this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+			this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+			this.imageList1.Images.SetKeyName(0, "FolderOpen");
+			this.imageList1.Images.SetKeyName(1, "FolderChoice");
+			// 
+			// lbl_FileChoice
+			// 
+			this.lbl_FileChoice.AutoSize = true;
+			this.lbl_FileChoice.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.lbl_FileChoice.Font = new System.Drawing.Font("굴림체", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+			this.lbl_FileChoice.ForeColor = System.Drawing.Color.Black;
+			this.lbl_FileChoice.Location = new System.Drawing.Point(86, 227);
+			this.lbl_FileChoice.Name = "lbl_FileChoice";
+			this.lbl_FileChoice.Size = new System.Drawing.Size(35, 16);
+			this.lbl_FileChoice.TabIndex = 7;
+			this.lbl_FileChoice.Text = "   ";
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.FileName = "openFileDialog1";
+			// 
+			// notifyIcon1
+			// 
+			this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+			this.notifyIcon1.Text = "notifyIcon1";
+			this.notifyIcon1.DoubleClick += new System.EventHandler(this.notifyIcon1_DoubleClick);
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = global::IPScanner.Properties.Resources.Folder_svg;
+			this.pictureBox1.Location = new System.Drawing.Point(41, 210);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(39, 41);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox1.TabIndex = 5;
+			this.pictureBox1.TabStop = false;
+			this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+			this.pictureBox1.MouseLeave += new System.EventHandler(this.pictureBox1_MouseLeave);
+			this.pictureBox1.MouseHover += new System.EventHandler(this.pictureBox1_MouseHover);
+			this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+			// 
+			// button2
+			// 
+			this.button2.Font = new System.Drawing.Font("굴림체", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+			this.button2.Location = new System.Drawing.Point(216, 68);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(70, 38);
+			this.button2.TabIndex = 8;
+			this.button2.Tag = "button2";
+			this.button2.Text = "설정";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// lblTmr
+			// 
+			this.lblTmr.AutoSize = true;
+			this.lblTmr.Font = new System.Drawing.Font("Georgia", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblTmr.ForeColor = System.Drawing.Color.Green;
+			this.lblTmr.Location = new System.Drawing.Point(132, 112);
+			this.lblTmr.Name = "lblTmr";
+			this.lblTmr.Size = new System.Drawing.Size(43, 41);
+			this.lblTmr.TabIndex = 30;
+			this.lblTmr.Text = "0";
+			this.lblTmr.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(411, 343);
+			this.Controls.Add(this.lblTmr);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.lbl_FileChoice);
+			this.Controls.Add(this.lbl_FilePath);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.numericUpDown1);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.button1);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MaximizeBox = false;
+			this.Name = "Form1";
+			this.Text = "통신 체커";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.Resize += new System.EventHandler(this.Form1_Resize);
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Timer Schedule_Timer;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.NumericUpDown numericUpDown1;
+		private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Label lbl_FilePath;
+		private System.Windows.Forms.ImageList imageList1;
+		private System.Windows.Forms.Label lbl_FileChoice;
+		private System.Windows.Forms.OpenFileDialog openFileDialog1;
+		public System.Windows.Forms.NotifyIcon notifyIcon1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Label lblTmr;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,439 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net.NetworkInformation;
+using System.Net;
+using System.Collections.Generic;
+using System.Xml;
+using Oracle.ManagedDataAccess.Client;
+using System.Data;
+
+/// <summary>
+/// IP 주소로 PC에 해당 포트가 오픈되어 있는지 확인. 
+/// </summary>
+
+namespace IPScanner
+{
+
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		// 버튼 클릭 해서 왔는지 체크 ( 설정 클릭시도 Tick작업을 하니까 )
+		//Boolean BtnCheck = false;
+		private long tmr = 0;
+		OracleConnection pgOraConn;
+		OracleCommand pgOraCmd;
+
+		// 구조체 선언
+		struct ItemInfo
+		{
+			public string Server;
+			public string Equip;
+			public string IP;
+			public int Port;
+
+		}
+
+		// 읽어온 정보를 담을 List 생성
+		List<ItemInfo> itemInfoList = new List<ItemInfo>();
+		private string _strConn = "Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=10.12.11.131)(PORT=1521)))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=orcl)));User ID=MESMGR2;Password=MESMGR2;Connection Timeout=30;";
+
+		// DB 연결
+		private bool ConnectionDBOpen(string dbIp, string dbName, string dbId, string dbPw)
+		{
+			bool retValue = false;
+			try
+			{
+				// 오라클 서버 연결 객체 생성
+				pgOraConn = new OracleConnection($"Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST={dbIp})(PORT=1521)))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME={dbName})));User ID={dbId};Password={dbPw};Connection Timeout=30;");
+				// OPEN
+				pgOraConn.Open();
+				// 명령 객체 생성
+				pgOraCmd = pgOraConn.CreateCommand();
+				// 연결 성공
+				retValue = true;
+			}
+			catch (Exception e)
+			{
+				// 연결 실패
+				retValue = false;
+				MessageBox.Show($"DB connection fail.\n {e.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
+
+			return retValue;
+		}
+
+		/// <summary>
+		/// 참고용으로만 적어 놓았음.
+		/// </summary>
+		/// <param name="StrQuery"></param>
+		private void DataRead(string StrQuery)
+		{
+
+			OracleCommand cmd = new OracleCommand();
+
+			cmd.Connection = pgOraConn;
+
+			cmd.CommandText = StrQuery; 
+
+			// 결과 리더 객체를 리턴
+			OracleDataReader rdr = cmd.ExecuteReader();
+
+			// 레코드 계속 가져와서 루핑
+
+			while (rdr.Read())
+			{
+
+				// 필드 데이타 읽기
+
+				string s = rdr["LOT_ID"] as string;
+
+				// 데이타를 리스트박스에 추가
+				//listBox1.Items.Add(s);
+
+			}
+
+			// 사용후 닫음
+			rdr.Close();
+		}
+
+		/// <summary>
+		/// Equip / IP / Port 를 받아 현재 상태를 DB에 저장함.
+		/// </summary>
+		/// <param name="Equip"></param>
+		/// <param name="IP"></param>
+		/// <param name="Port"></param>
+		private void DataInsert(string Server, string Equip, string IP, string Port, String LiveIP, String LivePort, String LiveConnection)
+		{
+			OracleTransaction STrans = null;  //오라클 트랜젝션
+
+			STrans = pgOraConn.BeginTransaction();
+
+			// 명령 객체 생성
+			OracleCommand cmd = new OracleCommand("", pgOraConn);
+			cmd.Connection = pgOraConn;
+
+			try
+			{
+				cmd.Transaction = STrans;  //커맨드에 트랜젝션 명시
+				// SQL문 지정 및 INSERT 실행
+				cmd.CommandText = "insert into IPStatus values ( NO_SEQ.NEXTVAL, '" + Server + "','" + Equip + "','" + IP + "','" + Port + "','" + LiveIP + "','" + LivePort + "','" + LiveConnection + "','" + System.DateTime.Now.ToString("yyyyMMddHHmmss") + "')";
+				cmd.ExecuteNonQuery();
+				// 커밋
+				cmd.Transaction.Commit();   
+			}
+
+			catch(Exception ex)
+			{
+				// 롤백
+				cmd.Transaction.Rollback();   
+			}
+			finally{
+				cmd.Dispose();
+			}
+
+		}
+
+		public void LoadXML()
+		{
+
+			// 읽어올 파일 경로
+			string fileAndPath = lbl_FileChoice.Text.ToString();
+
+			// XmlReader 로 xml 파일 읽어오기
+			XmlReader xmlReader;
+
+			try
+			{
+				if ( lbl_FileChoice.Text.Trim() == "")
+				{
+					MessageBox.Show("장비 설정 파일을 선택하시기 바랍니다.");
+					return;
+				}
+
+				XmlReaderSettings setting = new XmlReaderSettings();
+				setting.IgnoreComments = true;
+				setting.IgnoreWhitespace = true;
+				xmlReader = XmlReader.Create(fileAndPath, setting);
+			}
+			catch (System.Exception e)
+			{
+				return;
+			}
+
+			while (xmlReader.Read())   // xml 파일 노드 별로 읽기
+			{
+				// 읽은 노드가 Item 의 시작노드라면 정보 읽기
+				if (xmlReader.Name.CompareTo("Item".ToUpper()) == 0 &&
+					  xmlReader.NodeType == XmlNodeType.Element)
+				{
+					ItemInfo newItemInfo = new ItemInfo();
+
+					try
+					{
+						if (xmlReader.MoveToFirstAttribute())    // 첫 번째 속성이 있다면..
+						{
+							do
+							{
+								string AttribName = xmlReader.Name;
+
+								if (AttribName.CompareTo("Server".ToUpper()) == 0)
+									newItemInfo.Server = xmlReader.Value;
+								else if (AttribName.CompareTo("Equip".ToUpper()) == 0)
+									newItemInfo.Equip = xmlReader.Value;
+								else if (AttribName.CompareTo("IP".ToUpper()) == 0)
+									newItemInfo.IP = xmlReader.Value;
+								else if (AttribName.CompareTo("Port".ToUpper()) == 0)
+									newItemInfo.Port = int.Parse(xmlReader.Value);
+							} while (xmlReader.MoveToNextAttribute());
+							// 다음 속성으로 이동 & 있다면 반복..
+						}
+					}
+					catch (System.Exception e)
+					{
+						Console.WriteLine(e.Message);
+						Console.WriteLine(@"..\..\ItemInfo2.xml 아이템 테이블을 확인하세요.");
+						return;
+					}
+
+					// List 에 newItemInfo 추가
+					itemInfoList.Add(newItemInfo);
+				}
+			}
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+
+			try
+			{
+				// DB연결및 Open
+				ConnectionDBOpen("10.12.11.131", "orcl", "MESMGR2", "MESMGR2");
+
+				// 데이터 읽는 방법 기술 
+				// DataRead("SELECT * FROM CEDCINPFHS");
+
+				// IP리스트 xml 읽어오기
+				LoadXML();
+
+				Cursor.Current = Cursors.WaitCursor;
+
+				// List 에 저장된 정보 확인 ( 돌면서 확인 )
+				foreach (ItemInfo eachInfo in itemInfoList)
+
+				{
+					if (PingTest(eachInfo.IP))
+					{
+						// 해당 포트 테스트
+						if (ConnectTest(eachInfo.IP, eachInfo.Port))
+						{
+							// 인서트 처리 ( Ping도 되고 Port도 잘 된 경우 )
+							DataInsert(eachInfo.Server, eachInfo.Equip, eachInfo.IP, eachInfo.Port.ToString(),"Y","Y","Y");
+						}
+						else
+						{
+							// 인서트 처리 ( Ping은 되는데 Port가 막힌 경우 )
+							DataInsert(eachInfo.Server, eachInfo.Equip, eachInfo.IP, eachInfo.Port.ToString(), "Y", "N", "N");
+						}
+					}
+					else
+					{
+						// 인서트 처리 ( Ping도 안되고 Port도 막힌 경우 )
+						DataInsert(eachInfo.Server, eachInfo.Equip, eachInfo.IP, eachInfo.Port.ToString(), "N", "N", "N");
+					}
+				}
+
+				Cursor.Current = Cursors.Default;
+
+				// DB 닫기
+				pgOraCmd.Dispose();
+				pgOraConn.Close();
+
+			}
+			catch (Exception)
+			{
+
+			}
+			finally
+			{
+				pgOraCmd.Dispose();
+				pgOraConn.Close();
+			}
+		}
+
+		/// <summary>
+		///  Ping테스트하기
+		/// </summary>
+		/// <param name="ip"></param>
+		/// <returns></returns>
+		private static bool PingTest(string ip)
+		{
+			bool result = false;
+			try
+			{
+				Ping pp = new Ping();
+				PingOptions po = new PingOptions();
+				po.DontFragment = true;
+				byte[] buf = Encoding.ASCII.GetBytes("aaaaaaaaaaaaaaaaaaaa");
+				PingReply reply = pp.Send(IPAddress.Parse(ip), 10, buf, po);
+				if (reply.Status == IPStatus.Success)
+				{
+					result = true;
+				}
+				else
+				{
+					result = false;
+				}
+				return result;
+			}
+			catch
+			{
+				throw;
+			}
+		}
+
+		/// <summary>
+		/// ip와 포트를 이용하여 연결 테스트
+		/// </summary>
+		/// <param name="ip"></param>
+		/// <param name="port"></param>
+		/// <returns></returns>
+		private static bool ConnectTest(string ip, int port)
+		{
+			bool result = false; Socket socket = null;
+			try
+			{
+				socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+				socket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.DontLinger, false);
+				IAsyncResult ret = socket.BeginConnect(ip, port, null, null);
+				result = ret.AsyncWaitHandle.WaitOne(100, true);
+			}
+			catch
+			{ }
+			finally
+			{
+				if (socket != null)
+				{
+					socket.Close();
+				}
+			}
+			return result;
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			try
+			{
+				Schedule_Timer.Enabled = true;
+			}
+			catch
+			{
+
+			}
+		}
+
+		private void Schedule_Timer_Tick(object sender, EventArgs e)
+		{
+			tmr++;
+			lblTmr.Text = tmr.ToString();
+			// 1분계산
+			if ((tmr) > int.Parse(numericUpDown1.Value.ToString()) * 60)
+			{
+				// 타이머를 발동시킨다.
+				Schedule_Timer.Start();
+
+				// 시간마다 자동 시작
+				button1.PerformClick();
+			}
+		}
+
+		private void pictureBox1_Click(object sender, EventArgs e)
+		{
+			OpenFileDialog open = new OpenFileDialog();
+
+			open.Filter = "XML File(*.xml)|*.xml|AllFiles(*.*)|*.*";
+			open.InitialDirectory = @"C:\";
+			open.Title = "장비 설정 파일을 선택해 주십시오";
+
+			if (open.ShowDialog() == DialogResult.OK)
+			{
+				this.lbl_FileChoice.Text = open.FileName;
+			}
+
+		}
+
+		private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+		{
+			pictureBox1.Cursor = Cursors.Hand;
+		}
+
+		private void pictureBox1_MouseHover(object sender, EventArgs e)
+		{
+			pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+			pictureBox1.Image = Properties.Resources.Folder_sort_svg;
+
+		}
+
+		private void pictureBox1_MouseLeave(object sender, EventArgs e)
+		{
+			pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+			pictureBox1.Image = Properties.Resources.Folder_svg;
+
+		}
+
+		private void button1_MouseMove(object sender, MouseEventArgs e)
+		{
+			button1.Cursor = Cursors.Hand;
+		}
+
+		private void notifyIcon1_DoubleClick(object sender, EventArgs e)
+		{
+			// 시스템 트레이에서의 아이콘이 더블클릭되었을때의 이벤트
+
+			// 윈도우의 모습을 보인다.
+			this.Visible = true;
+			// 아이콘의 모습을 보인다.
+			this.ShowIcon = true;
+			// 시스템트레이의 모습을 감춘다.
+			notifyIcon1.Visible = false;
+			// 윈도우 중간으로 나오도록
+			this.WindowState = FormWindowState.Normal;
+
+		}
+
+		private void Form1_Resize(object sender, EventArgs e)
+		{
+			// winform의 최소화버튼을 눌렀을때의 이벤트
+			if (this.WindowState == FormWindowState.Minimized)
+			{
+				// 윈도우의 모습을 감춘다.				
+				this.Visible = false;
+				// 아이콘의 모습을 감춘다.
+				this.ShowIcon = false;
+				// 시스템트레이의 모습을 보인다.
+				notifyIcon1.Visible = true; 
+				}
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			if (lbl_FileChoice.Text.Trim() == "")
+			{
+				MessageBox.Show("장비 설정 파일을 선택하시기 바랍니다.");
+				return;
+			}
+			else
+			{
+				// 초기화
+				lblTmr.Text = "0";
+				tmr = 0;
+				Schedule_Timer.Enabled = true;
+			}
+		}
+	}
+}
